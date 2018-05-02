bb6d84f11f9c70ab65576f3aa44848e743150577
kahmannf <felix@kahmann.de>
2018-05-02 19:52:46 +0200

    add router, home-component and navbar-component

---
+++ universe-manager-client/src/app/app-routes.ts
@@ -0,0 +1,8 @@
+import { Route } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+
+
+
+export const appRoutes: Route[] = [
+    { path: 'home', component: HomeComponent }
+]
--- universe-manager-client/src/app/app.module.ts
+++ universe-manager-client/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
-
+/* components */
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
+/* other */
+import { appRoutes } from './app-routes';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {
+      enableTracing: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
